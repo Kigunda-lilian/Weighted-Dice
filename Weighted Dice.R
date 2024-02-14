a1a0c5a9a44a76db2a1908efee6f459cca8481ec
Lilian <kigundanashlil@gmail.com>
2024-02-14 17:53:54 +0300

    add introduction

---
+++ Weighted Dice.R
@@ -0,0 +1,12 @@
+# ££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££
+# Practising R programming using the following platforms: 
+# - afterwork.io,
+# - rstudio-education.github.io
+ # Part 1 : - Weighted Dice project
+ #           -Introduction to R programming project
+#  Date: 12th Feb 2024
+# By Lilian Kigunda
+# ££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££
+
+# The colon operator (:) returns every integer between two integers.
+# It is an easy way to create a sequence of numbers.
