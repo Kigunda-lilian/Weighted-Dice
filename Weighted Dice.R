a8a8d4ca74cf4998a56331bdac65f71b570b8b14
Lilian <kigundanashlil@gmail.com>
2024-02-14 18:28:05 +0300

    add comments

--- Weighted Dice.R
+++ Weighted Dice.R
@@ -6,7 +6,10 @@
  #           -Introduction to R programming project
 #  Date: 12th Feb 2024
 # By Lilian Kigunda
+#This is my first R script to push to github!!! I am super excited
+# Let's do this !!!!!!!!
 # ££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££
 
 # The colon operator (:) returns every integer between two integers.
 # It is an easy way to create a sequence of numbers.
+# You can do inner multiplication with the %*% operator and outer multiplication with the %o% operator:
