076655a23792c908db429a9b410d133647b6bbca
unknown <liliankanana96@gmail.com>
2024-02-21 15:22:03 +0300

    add playing cards rscript

---
+++ playing cards.R
@@ -0,0 +1,5 @@
+# ######################################################################### 
+# Playing cards 
+# project 2
+#This project will teach me how to store, retrieve, 
+# and change data values in my computer’s memory
