4c8831b495816406a06cdc8dfe3a3bcb2d5f30fe
unknown <liliankanana96@gmail.com>
2024-02-22 10:59:23 +0300

    exercise 5.3

--- playing cards.R
+++ playing cards.R
@@ -4,13 +4,18 @@
 #This project will teach me how to store, retrieve, 
 # and change data values in my computer’s memory
 
-# Build the deck
+# BUILD THE DECK
 # matrix will fill up the matrix column by column by default, 
 # but you can fill the matrix row by row if you include the argument byrow = TRUE:
 
 die <- 1:6
 matrix(die,nrow = 2,byrow = T)
-
+# Arrays
+# array is not as customizeable as matrix and 
+# basically does the same thing as setting the dim attribute
+# To use array, provide an atomic vector as the first argument,
+# and a vector of dimensions as the second argument, now called dim:
+# 
 
 # Write functions that deal and shuffle
 # Change the point system to suit your game
