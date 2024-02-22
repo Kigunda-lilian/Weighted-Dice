fa7bd720a2106d9333f3969b80a1ec53df751215
unknown <liliankanana96@gmail.com>
2024-02-22 10:43:36 +0300

    add matrices

--- playing cards.R
+++ playing cards.R
@@ -3,3 +3,16 @@
 # project 2
 #This project will teach me how to store, retrieve, 
 # and change data values in my computer’s memory
+
+# Build the deck
+# matrix will fill up the matrix column by column by default, 
+# but you can fill the matrix row by row if you include the argument byrow = TRUE:
+
+die <- 1:6
+matrix(die,nrow = 2,byrow = T)
+
+
+# Write functions that deal and shuffle
+# Change the point system to suit your game
+# Manage the state of the deck
+# 
