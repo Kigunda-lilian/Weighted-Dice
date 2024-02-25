4014d49fccba1ecce479b71792b46fc073d338aa
unknown <liliankanana96@gmail.com>
2024-02-25 06:37:32 +0300

    add project3

---
+++ Slot machine.R
@@ -0,0 +1,15 @@
+# ######################################################################### 
+# Slot machine 
+# project 3
+#This project will teach me how to write programs and run simulations in R. 
+#I will also learn how to:
+
+#Use a practical strategy to design programs
+#Use if and else statements to tell R what to do when
+#Create lookup tables to find values
+#Use for, while, and repeat loops to automate repetitive operations
+#Use S3 methods, R’s version of Object-Oriented Programming
+#Measure the speed of R code
+#Write fast, vectorized R code
+# setting the language in the system environment to english:Sys.setenv(LANG = "en")
+############################################################################
