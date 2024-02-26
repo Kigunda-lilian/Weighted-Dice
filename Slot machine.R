8035961e92654bb65f7773da9481f2ae9a7ad3a9
unknown <liliankanana96@gmail.com>
2024-02-26 18:05:20 +0300

    add function

--- Slot machine.R
+++ Slot machine.R
@@ -79,3 +79,45 @@ symbols_3[1] == "B" | symbols_3[1] == "BB" | symbols_3[1] == "BBB" &
 # You can often replace multiple | operators with a single %in%. Also, you can check that a test is true for each element in a vector with all. 
 # These two changes shorten the preceding code to:
 all(symbols_3 %in% c("B", "BB", "BBB")) #always remember that what is being tested is on the left
+bars <- symbols_3 %in% c("B", "BB", "BBB")
+
+symbols <- c("dd","B","7") 
+same <- symbols[1] == symbols[2] && symbols[2] == symbols[3]
+bars <- symbols %in% c("B", "BB", "BBB")
+
+if (same) {
+  payouts <- c("DD" = 100, "7" = 80, "BBB" = 40, "BB" = 25, 
+               "B" = 10, "C" = 10, "0" = 0)
+  prize <- unname(payouts[symbols[1]])
+} else if (all(bars)) {
+  prize <- 5
+} else {
+  cherries <- sum(symbols == "C")
+  prize <- c(0, 2, 5)[cherries + 1]
+}
+
+diamonds <- sum(symbols == "DD")
+prize * 2 ^ diamonds
+
+
+score <- function (symbols) {
+  # identify case
+  same <- symbols[1] == symbols[2] && symbols[2] == symbols[3]
+  bars <- symbols %in% c("B", "BB", "BBB")
+  
+  # get prize
+  if (same) {
+    payouts <- c("DD" = 100, "7" = 80, "BBB" = 40, "BB" = 25, 
+                 "B" = 10, "C" = 10, "0" = 0)
+    prize <- unname(payouts[symbols[1]])
+  } else if (all(bars)) {
+    prize <- 5
+  } else {
+    cherries <- sum(symbols == "C")
+    prize <- c(0, 2, 5)[cherries + 1]
+  }
+  
+  # adjust for diamonds
+  diamonds <- sum(symbols == "DD")
+  prize * 2 ^ diamonds
+}
